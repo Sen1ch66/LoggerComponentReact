b4c171bf6ce00cf026183cb63c88a5c23946f338
Sen1ch66 <arsenzahovajko@gmail.com>
2024-10-11 21:47:31 +0200

    new work commit

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,11 +1,19 @@
- import Button from "@mui/material/Button";
+import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import { useEffect, useState } from "react";
 function Counter() {
   let [count, setCount] = useState(0)
+  const date = new Date()
   useEffect(()=> {
-    console.log('Компонент змонтувався')
-  }, [])
+    console.log('Компонент змонтувався' + date.toISOString())
+    console.log('Компонент оновився' + date.toISOString())
+    return function unMount(){
+      console.log('Я розмонтувався'+ date.toISOString())
+    }
+  }, [count])
+  function deleteComp(e){
+      e.preventDefault()
+  }
   return (
     <div className="App">
       <h1>First component</h1>
@@ -14,6 +22,7 @@ function Counter() {
         <Button variant="text" className="button" onClick={()=> setCount(++count)}>increase</Button>
         <Button variant="text" className="button" onClick={()=> setCount(--count)}>decrease</Button>
         <Button variant="outlined" onClick={()=> setCount(0)}>Reset</Button>
+        <button style={{marginTop: '20px', padding: '10px 20px', background: 'grey'}} onClick={deleteComp}>Hide component</button>
       </Card>
     </div>
   );
